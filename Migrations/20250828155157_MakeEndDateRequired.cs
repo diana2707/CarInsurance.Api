03dfd433907734eb2bcfc4aa468a7c9a8a60907b
diana2707 <61651284+diana2707@users.noreply.github.com>
2025-08-28 21:02:14 +0300

    make EndDate required by updating the database and the model (#1)

---
+++ Migrations/20250828155157_MakeEndDateRequired.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarInsurance.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeEndDateRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE Policies SET EndDate = '2025-12-31' WHERE EndDate IS NULL;");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EndDate",
+                table: "Policies",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1),
+                oldClrType: typeof(DateOnly),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "EndDate",
+                table: "Policies",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "TEXT");
+        }
+    }
+}
